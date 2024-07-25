2e1658cbad5e6308c567c1c9c3a0c75a25fc3815
salmakinanthi <22611025@students.uii.ac.id>
2024-07-26 02:28:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 import streamlit as st
+import joblib
+import requests
 import pandas as pd
-import numpy as np
-import os
+from io import BytesIO
 
-# Load the model
-model_path = 'model.pkl'
-if not os.path.isfile(model_path):
-    raise FileNotFoundError(f"Model file {model_path} does not exist.")
-model = joblib.load(model_path)
+# URLs to the files on GitHub
+MODEL_URL = "https://raw.githubusercontent.com/salmakinanthi/uasmpml/master/best_model.pkl"
+
+def load_file_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        return BytesIO(response.content)
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error downloading file from {url}: {e}")
+        return None
+
+# Load the model directly from URL
+model_file = load_file_from_url(MODEL_URL)
+
+if model_file is not None:
+    try:
+        model, feature_names = joblib.load(model_file)
+        st.write("Model loaded successfully.")
+    except Exception as e:
+        st.error(f"An error occurred while loading the model: {e}")
+else:
+    model = None
+
 
 
 # Function to convert categorical data to numerical data using Ordinal Encoding
